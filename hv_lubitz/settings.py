15fdebcc4820473ab109dfd6ff481ee8c2959cde
Timo Lubitz <timo.lubitz@gmail.com>
2018-10-30 10:55:49 +0100

    deploy test

--- hv_lubitz/settings.py
+++ hv_lubitz/settings.py
@@ -139,7 +139,7 @@ DATABASES['default'].update(db_from_env)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
