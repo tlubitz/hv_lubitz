c34ec8117864d293337106267317cb5737d4968b
Timo Lubitz <timo.lubitz@gmail.com>
2018-10-30 10:53:43 +0100

    deploy test

--- hv_lubitz/settings.py
+++ hv_lubitz/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='kbf5q6eaeMgEJyK4fkNLmNnHh38xh38MK4hSJwa9c4veMYwvre'
-#SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+#SECRET_KEY ='kbf5q6eaeMgEJyK4fkNLmNnHh38xh38MK4hSJwa9c4veMYwvre'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
-DEBUG = True
+DEBUG = False
+#DEBUG = True
 
 ALLOWED_HOSTS = ['fierce-thicket-13876.herokuapp.com','127.0.0.1']
 
